3eaf2d2677574d7d3f77bdbaaa78c7bc943d6856
Evandro Rhari <93932260+E-rhari@users.noreply.github.com>
2024-03-28 20:08:59 -0300

    NoteObject

---
+++ GamethONS_Case/Assets/Scripts/RythmScripts/NoteObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool hittable = false;
+    private HitController hit;
+
+    void Start()
+    {
+        hit = FindObjectOfType<HitController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 beatPosition = transform.position;
+        Vector3 hitPosition  = hit.transform.position;
+        float distanceDifference = Mathf.Abs(hitPosition.x - beatPosition.x);
+        if(distanceDifference <= 2)
+            GetComponent<BeatScroller>().hasStarted = false;
+        
+    }
+}
